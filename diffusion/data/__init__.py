e967e30652bdd85d80724c3793cecd202126c5a8
wangzhj <zhoujiewang@njust.edu.cn>
2022-08-27 13:37:26 +0000

    rewrite snow100K for train and val

--- diffusion/data/__init__.py
+++ diffusion/data/__init__.py
@@ -4,4 +4,4 @@ from .all_weather import AllWeatherDataset
 from .base import BaseDataset
 from .cifar10 import CIFAR10
 from .raindrop import RaindropValDataset
-from .snow100k import SnowValDataset
+from .snow100k import Snow100K
--- diffusion/data/snow100k.py
+++ diffusion/data/snow100k.py
@@ -5,21 +5,25 @@ from typing import Callable, Optional
 from .base import BaseDataset
 
 
-class SnowValDataset(BaseDataset):
+class Snow100K(BaseDataset):
     def __init__(
         self, 
-        root: str = '/nas/datasets/weather/snow100k/test/Snow100K-L', 
+        root: str = '/nas/datasets/weather/snow100k', 
         patch_size: int = 64, 
         n: int = 16, 
         transform: Optional[Callable] = None, 
         parse_patches: bool = True,
         num_timesteps: int = 1000,
+        mode: str = 'train',
     ):
+        assert mode in ['train', 'L', 'M', 'S']
         super().__init__(root, patch_size, n, transform, 
                          parse_patches, num_timesteps)
-
+        rel_path = {'train': 'train', 'L': 'test/Snow100K-L', 
+                    'M': 'test/Snow100K-M', 'S': 'test/Snow100K-S'}
+        self.root = os.path.join(self.root, rel_path[mode])
         self.input_paths = sorted(glob.glob(f"{self.root}/synthetic/*"))
-        print(f'Snow100k val dataset size: {len(self.input_paths)}.')
+        print(f'Snow100k-{mode} dataset size: {len(self.input_paths)}.')
     
     def get_paths_and_name(self, idx):
         input_path = self.input_paths[idx]
