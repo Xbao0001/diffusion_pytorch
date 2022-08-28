5687032d44de4c00bcad4709ade67396517ed7c5
wangzhj <zhoujiewang@njust.edu.cn>
2022-08-28 14:46:59 +0000

    finished patched_ddim

--- diffusion/pipelines/patched_ddim/pipeline_patched_ddim.py
+++ diffusion/pipelines/patched_ddim/pipeline_patched_ddim.py
@@ -14,60 +14,95 @@
 # limitations under the License.
 
 
-import warnings
-
 import torch
-
-from tqdm.auto import tqdm
-
+import torch.nn.functional as F
 from diffusers.pipeline_utils import DiffusionPipeline
+from einops import rearrange
+from tqdm.auto import tqdm
 
 
 class PatchedDDIMPipeline(DiffusionPipeline):
-    def __init__(self, unet, scheduler):
+    def __init__(self, unet, scheduler, kernel_size=64, stride=16, batch_size=128):
         super().__init__()
+
         scheduler = scheduler.set_format("pt")
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.batch_size = batch_size
         self.register_modules(unet=unet, scheduler=scheduler)
 
     @torch.no_grad()
-    def __call__(self, batch_size=1, generator=None, eta=0.0, num_inference_steps=50, output_type="pil", **kwargs):
-
-        if "torch_device" in kwargs:
-            device = kwargs.pop("torch_device")
-            warnings.warn(
-                "`torch_device` is deprecated as an input argument to `__call__` and will be removed in v0.3.0."
-                " Consider using `pipe.to(torch_device)` instead."
-            )
-
-            # Set device as before (to be removed in 0.3.0)
-            if device is None:
-                device = "cuda" if torch.cuda.is_available() else "cpu"
-            self.to(device)
+    def __call__(
+        self,
+        shape,
+        cond=None,
+        generator=None,
+        torch_device=None,
+        eta=0.0,
+        num_inference_steps=50,
+        output_type=None,
+    ):
+        B, C, H, W = shape
 
         # eta corresponds to η in paper and should be between [0, 1]
+        if torch_device is None:
+            torch_device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.unet.to(torch_device)
 
-        # Sample gaussian noise to begin loop
-        image = torch.randn(
-            (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size),
-            generator=generator,
-        )
-        image = image.to(self.device)
+        # Sample gaussian noise to begin loop, if has cond, concatenate
+        # them in channel dim. image.shape = B C H W, C = 6 if concatenated
+        image = torch.randn(shape, generator=generator).to(torch_device)
+        if cond is not None:
+            cond = cond.to(torch_device)
+
+        # create mask for overlaped patches
+        mask = torch.ones((1, 1, H, W)).to(torch_device)
+        mask = mask.unfold(2, self.kernel_size, self.stride
+                           ).unfold(3, self.kernel_size, self.stride)
+        mask = rearrange(mask, 'b c nh nw h w -> b (c h w) (nh nw)')
+        mask = F.fold(mask, output_size=(H, W), kernel_size=self.kernel_size,
+                      stride=self.stride)
 
         # set step values
         self.scheduler.set_timesteps(num_inference_steps)
 
         for t in tqdm(self.scheduler.timesteps):
-            # 1. predict noise model_output
+            # 1. create overlapped patches from images
+            # https://discuss.pytorch.org/t/how-to-split-tensors-with-overlap-and-then-reconstruct-the-original-tensor/70261/2
 
-            model_output = self.unet(image, t)["sample"]
+            if cond is not None:
+                image = torch.cat([image, cond], dim=1)
+            patches = image.unfold(2, self.kernel_size, self.stride
+                                   ).unfold(3, self.kernel_size, self.stride)
+            patches = rearrange(patches, 'b c nh nw h w -> (b nh nw) c h w')
+            patch_list = torch.split(patches, self.batch_size, dim=0)
+            
+            res = []
+            for patch in patch_list:
+                # 2. predict noise from patches
+                # NOTE: suppose img and cond have the same shape
+                img, c = torch.chunk(patch, 2, dim=1)
+                model_output = self.predict(img, c, t)
+                # 3. get previous patches' mean and add variance depending on eta
+                patch = self.scheduler.step(model_output, t, img, eta)["prev_sample"]
+                res.append(patch)
 
-            # 2. predict previous mean of image x_t-1 and add variance depending on eta
-            # do x_t -> x_t-1
-            image = self.scheduler.step(model_output, t, image, eta)["prev_sample"]
+            patches = torch.vstack(res)
 
-        image = (image / 2 + 0.5).clamp(0, 1)
-        image = image.cpu().permute(0, 2, 3, 1).numpy()
+            # 4. restore the whole image
+            image = rearrange(patches, '(b k) c h w -> b (c h w) k', b=B)
+            image = F.fold(image, (H, W), self.kernel_size, stride=self.stride)
+            image /= mask
+
+        image = (image / 2 + 0.5).clamp(0, 1).cpu()
         if output_type == "pil":
-            image = self.numpy_to_pil(image)
+            image = self.numpy_to_pil(image.permute(0, 2, 3, 1).numpy())
+        elif output_type == 'numpy':
+            image = image.numpy()
 
         return {"sample": image}
+
+    def predict(self, img, cond, t):
+        if cond is not None:
+            input = torch.cat([img, cond], dim=1)
+        return self.unet(input, t)['sample']
